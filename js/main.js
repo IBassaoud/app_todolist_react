cd376d6f5a098369381b6b5d83fe4702fe809882
ismail BASSAOUD <bwb-i.bassaoud@fondespierre.com>
2022-09-27 21:06:46 +0200

    Css style & moved code from render parent to child component

--- js/main.js
+++ js/main.js
@@ -2,9 +2,6 @@ const header = document.getElementById('header');
 const main = document.getElementById('main');
 const footer = document.getElementById('footer');
 
-const createTask = document.getElementById('create_task');
-const allTasks = document.getElementById('all_tasks');
-
 class CreateTaskComponent extends React.Component {
     state = {
         inputTask: React.createRef(),
@@ -16,10 +13,10 @@ class CreateTaskComponent extends React.Component {
         ],
     }
 
+    // Creation task handler
     handleCreationTask = event => {
         let newTask;
-
-        if (this.state.tasks.length == 0){
+        if (this.state.tasks.length == 0) {
             newTask = {
                 name: this.state.inputTask.current.value,
                 state: "status-todo",
@@ -36,7 +33,6 @@ class CreateTaskComponent extends React.Component {
                 key: lastElementKey + 1
             }
         }
-       
 
         // Update the task list 
         if (this.state.inputTask.current.value != '') {
@@ -48,22 +44,33 @@ class CreateTaskComponent extends React.Component {
         this.state.inputTask.current.value = ""
     }
 
+    // Delete task handler
     handleDeleteTask = (key) => () => {
         // Delete the task 
         this.setState({ tasks: this.state.tasks.filter(task => task.key != key) });
+        console.log(this.state.tasks);
     }
 
+    // Edit task handler
+    handleEditTask = (key) => () => {
+        // Edit the task 
+
+        // this.setState({ tasks: this.state.tasks.filter(task => task.key != key) });
+        console.log(this.state.tasks);
+    }
+
+
     render() {
-        const input = <input type="text" id="creation_task_input" name="creation_task" ref={this.state.inputTask} onKeyPress={event => { if (event.key === "Enter") this.handleCreationTask() }} />
-        const button = <input type="button" id="creation_task_button" value="Create" onClick={this.handleCreationTask} />
+        const input = <input type="text" className="input_create" ref={this.state.inputTask} onKeyPress={event => { if (event.key === "Enter") this.handleCreationTask() }} />
+        const button = <input type="button" className="button_create" value="Create" onClick={this.handleCreationTask} />
         return (
-            <div id="create_task">
+            <div className="container">
                 {input}
                 {button}
-                <div id="all_tasks">
+                <div className="task">
                     <table>
                         <thead>
-                            <tr className="thead_row">
+                            <tr>
                                 <th>Nom de la tâche</th>
                                 <th>Statut</th>
                                 <th>Actions</th>
@@ -71,31 +78,9 @@ class CreateTaskComponent extends React.Component {
                         </thead>
                         <tbody>
                             {
+                                // Map function for each task in the array generate a component 
                                 this.state.tasks.map((task, index) =>
-                                    <tr key={task.key} className="table_row">
-                                        <RenderCreatedTaskName name={task.name} />
-                                        <RenderCreatedTaskStatus state={task.state} stateText={task.stateText} />
-                                        <td className="actions">
-                                            <div className="actions_buttons">
-                                                <div className="actions_item">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
-                                                    </svg>
-                                                </div>
-                                                <div className="actions_item">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
-                                                    </svg>
-                                                </div>
-                                                <div className="actions_item" onClick={this.handleDeleteTask(task.key)}>
-                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                                                    </svg>
-                                                </div>
-                                            </div>
-                                        </td>
-                                    </tr>
+                                    <RenderCreatedTaskComponent key={index} task={task} deleteTheTask={this.handleDeleteTask} />
                                 )
                             }
                         </tbody>
@@ -106,21 +91,37 @@ class CreateTaskComponent extends React.Component {
     }
 };
 
-function RenderCreatedTaskName(props) {
+// Child component : generate a table row for each task 
+function RenderCreatedTaskComponent(props) {
     return (
-        <td className="text-center task_name">{props.name}</td>
+        <tr>
+            <td className="text-center task-item__name">{props.task.name}</td>
+            <td>
+                <div className={props.task.state}>{props.task.stateText}</div>
+            </td>
+            <td>
+                <div className="action">
+                    <div className="action_item">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                        </svg>
+                    </div>
+                    <div className="action_item">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+                        </svg>
+                    </div>
+                    <div className="action_item" onClick={props.deleteTheTask(props.task.key)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                        </svg>
+                    </div>
+                </div>
+            </td>
+        </tr>
     )
 }
-function RenderCreatedTaskStatus(props) {
-    return (
-        <td>
-            <div className={props.state}>{props.stateText}</div>
-        </td>
-    )
-}
-
-ReactDOM.render(<CreateTaskComponent></CreateTaskComponent>, main)
-
 
 /* ########## ALTERNATIVE SYNTAX ########## */
 
@@ -130,24 +131,24 @@ ReactDOM.render(<CreateTaskComponent></CreateTaskComponent>, main)
 //         return (
 
 //             <tr>
-//                 <td className="text-center task_name">{this.props.task.name}</td>
+//                 <td className="text-center task-item__name">{this.props.task.name}</td>
 //                 <td>
 //                     <div className={this.props.task.state}>{this.props.task.stateText}</div>
 //                 </td>
-//                 <td className="actions">
-//                     <div className="actions_buttons">
-//                         <div className="actions_item">
+//                 <td className="action">
+//                     <div className="action">
+//                         <div className="action_item">
 //                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 //                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
 //                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
 //                             </svg>
 //                         </div>
-//                         <div className="actions_item">
+//                         <div className="action_item">
 //                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 //                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
 //                             </svg>
 //                         </div>
-//                         <div className="actions_item">
+//                         <div className="action_item" onClick={this.props.deleteTheTask(this.props.task.key)}>
 //                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 //                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
 //                             </svg>
@@ -161,3 +162,4 @@ ReactDOM.render(<CreateTaskComponent></CreateTaskComponent>, main)
 // }
 
 
+ReactDOM.render(<CreateTaskComponent></CreateTaskComponent>, main)
